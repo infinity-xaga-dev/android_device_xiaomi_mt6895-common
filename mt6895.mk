88fdf4c6ab6fbc24cba62b4ad5c7f04940534dcf
Mashopy <eliasgheeraert@gmail.com>
2025-06-15 10:53:15 +0530

    mt6895-common Track com.mediatek.ims.config.xml sysconfig
    This add the IMS package into the whitelist for ignoring location settings (emergency purposes).
    
    Change-Id: If2e59ed117bef992d55eb429d0a0f3814a20c6b9

--- mt6895.mk
+++ mt6895.mk
@@ -136,7 +136,8 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml \
+    $(LOCAL_PATH)/configs/permissions/com.mediatek.ims.config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/com.mediatek.ims.config.xml
 
 # Keymaster
 PRODUCT_PACKAGES += \
